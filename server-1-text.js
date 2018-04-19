4443365393f2f7d2b42caff80f1ab11205a57fdc
Jake Krijnen <jakekrijnen@live.nl>
2018-04-19 15:53:46 +0200

    Port toegevoegd

--- server-1-text.js
+++ server-1-text.js
@@ -3,6 +3,8 @@
 //
 var http = require('http');
 
+const port = process.env.PORT || 3000;
+
 http.createServer(function (request, response){
     console.log('Er was een request. ');
     response.writeHead(200, {'Content-Type': 'application/json'});
@@ -10,7 +12,8 @@ http.createServer(function (request, response){
         'firstname': 'Jake',
         'lastname': 'Krijnen'
     };
-    response.write(JSON.stringify(result)).end();
+    response.write(JSON.stringify(result));
+    response.end();
 }).listen(3000);
 
 console.log('De server luistert op port 3000');
