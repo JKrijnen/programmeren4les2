492a48714c8152d874b333e7da2095b5e343d81a
Jake Krijnen <jakekrijnen@live.nl>
2018-04-19 15:27:47 +0200

    Initial commit

---
+++ server-1-text.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+http.createServer(function (request, response){
+    console.log('Er was een request. ');
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    let result = {
+        'firstname': 'Jake',
+        'lastname': 'Krijnen'
+    };
+    response.write(JSON.stringify(result)).end();
+}).listen(3000);
+
+console.log('De server luistert op port 3000');
